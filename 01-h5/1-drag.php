fdef0444389f2b32f8cc193138fdcb1cc754d669
mzs55557 <891489656@qq.com>
2018-10-10 12:28:17 +0800

    as

--- 01-h5/1-drag.php
+++ 01-h5/1-drag.php
@@ -6,4 +6,16 @@
  * Date: 2018/9/23 0023
  * Time: 20:34
  */
-
+$arr = array(
+    array(
+        'id' => 1,
+        'name' => 'mzs',
+        'age' => 18
+    ),
+    array(
+        'id' => 2,
+        'name' => '心艾',
+        'age' => 16
+    )
+);
+echo json_encode($arr);
